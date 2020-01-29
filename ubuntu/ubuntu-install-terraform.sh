51a8eb2bb1f19771539a4bd18793eacb5ad45e84
Tom Atwood <thomasatwood@ipsidy.com>
2020-01-28 21:15:04 -0500

    #14: Updated Terraform to 0.12.20.

--- ubuntu/ubuntu-install-terraform.sh
+++ ubuntu/ubuntu-install-terraform.sh
@@ -10,9 +10,9 @@ NC='\033[0m' # No Color
 echo -e "\n${RED}Running installation of Terraform...${NC}\n"
 
 # Set version to download
-VERSION="0.12.19"
+VERSION="0.12.20"
 
-# Save current directory
+# Save current d    irectory
 CURRENT_DIR=$(pwd)
 
 cd ~

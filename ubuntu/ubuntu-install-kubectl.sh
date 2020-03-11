532e63ab0864ebed2dc9e7c3ad3e299f7ee19b2f
ajp-lqx <50920681+ajp-lqx@users.noreply.github.com>
2020-03-11 11:46:46 -0400

    Changed chmod to 700

--- ubuntu/ubuntu-install-kubectl.sh
+++ ubuntu/ubuntu-install-kubectl.sh
@@ -28,7 +28,7 @@ kubectl config use-context docker-for-desktop
 mkdir k9s
 curl -Ls https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_x86_64.tar.gz --output k9s/k9s.tar.gz
 tar -xzvf ./k9s/k9s.tar.gz -C ./k9s
-chmod 751 ./k9s/k9s
+chmod 700 ./k9s/k9s
 sudo mv ./k9s/k9s /usr/local/bin/k9s
 rm -rf k9s
 
